bed275dcc3bf0744154c8645e5b7d9d18a5c73e4
gleiSUN <go5624314@gmail.com>
2025-06-20 00:26:10 -0300

    Correção colisão player e movimentação no ar

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -3,19 +3,21 @@ extends CharacterBody2D
 @onready var Animacoes = $body/Animacoes
 
 const gravity = 1000
-const speed = 300
-const jump = -300
-const jump_horizontal = 100
+@export var speed: int = 300
+@export var jump: int = -300
+@export var jump_horizontal = 100
+
 var direction = 0
 
 enum states {idle, run, jump, dash, death}
 
-var current_state
+var current_state: states
 
 func _ready():
 	current_state = states.idle
 	
-func _physics_process(delta):
+func _physics_process(delta: float):
+	player_movements()
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
@@ -25,37 +27,27 @@ func _physics_process(delta):
 	
 	player_animations()
 	
-func player_falling(delta):
+func player_falling(delta: float):
 	if !is_on_floor():
 		velocity.y += gravity * delta
 		
-func player_idle(delta):
+func player_idle(delta: float):
 	if is_on_floor():
 		current_state = states.idle
 
-func player_run(delta):
+func player_run(delta: float):
 	if !is_on_floor():
 		return
 		
-	direction = Input.get_axis("move_left","move_right")
-	
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x,0,speed)
-		
 	if direction != 0:
 		current_state = states.run
-		Animacoes.flip_h = false if direction > 0 else true
 		
-func player_jump(delta):
+func player_jump(delta: float):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump
 		current_state = states.jump
 	if !is_on_floor() and current_state == states.jump:
-		direction = Input.get_axis("move_left","move_right")
 		velocity.x += direction * jump_horizontal * delta
-
 		
 func player_animations():
 	if current_state == states.idle:
@@ -64,5 +56,12 @@ func player_animations():
 		Animacoes.play("run")
 	elif current_state == states.jump:
 		Animacoes.play("jump")
-
+		
+func player_movements():
+	direction = Input.get_axis("move_left","move_right")
 	
+	if direction:
+		velocity.x = direction * speed
+		$body.scale.x = -1 if direction < 0 else 1
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
